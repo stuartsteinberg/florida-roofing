9178a4d8a90b29a5955241cdc5203f83c0927502
Mohid Meer <mohidfauji@gmail.com>
2024-12-06 05:23:58 +0500

    branding:updated logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='bg-primary text-4xl font-extrabold px-4 w-fit  '>
-      B
+    <div className='text-4xl font-extrabold px-4 w-fit flex-shrink-0  '>
+      <Image src={'/logo.jpg'} width={150} height={20}  />
     </div>
   )  
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -62,25 +62,25 @@ export default function NavBar() {
   const [open, setOpen] = useState(false)
   return (
     <div className=" text-white top-0 left-0 h-26  absolute w-full z-50  p-2 " >
-      <nav className="flex justify-between max-w-6xl mx-auto items-center  relative mt-20 ">
+      <nav className="flex justify-between max-w-6xl mx-auto items-center  relative mt-20 gap-2  ">
         <Logo />
 
         <ul className={`
-          sm:flex sm:flex-row sm:w-full sm:overflow-visible sm:translate-y-0   sm:translate-x-0  sm:h-full sm:static sm:bg-transparent sm:text-white sm:gap-x-8 
-            rounded-md  gap-y-6  items-center flex absolute  -translate-y-[-60%] left-1/2 -translate-x-[50%]   flex-col justify-center w-full transition-all duration-300 ${open ?  'min-h-fit p-2' : 'h-0 w-0 p-0' }  bg-white text-black overflow-hidden`}>
+          md:flex md:flex-row md:w-full md:overflow-visible md:translate-y-0   md:translate-x-0  md:h-full md:static md:bg-transparent md:text-white md:gap-x-4 lg:gap-x-8 
+            rounded-md  gap-y-6  items-center flex absolute  -translate-y-[-60%] left-1/2 -translate-x-[50%]   flex-col justify-center w-full transition-all duration-300 ${open ?  'min-h-fit p-2 bg-white' : 'h-0 w-0 p-0' }   text-black overflow-hidden`}>
           {navigation_links.map((item, index) => (
-           <li key={index} className="text-sm group relative w-full sm:w-fit">
+           <li key={index} className="text-sm group relative w-full md:w-fit whitespace-nowrap">
            {/* Parent Link */}
            <Link
              href={item.href}
-             className="hover:underline underline-offset-1 decoration-black font-bold block"
+             className="hover:underline underline-offset-1 decoration-black lg:font-bold block"
            >
              {item.label}
            </Link>
          
            {/* Submenu */}
            {item.sub_menu && (
-             <div className="absolute sm:left-0 mt-2 z-10 ">
+             <div className="absolute md:left-0 mt-2 z-10 ">
                <ul className="opacity-0 group-hover:opacity-100 rounded-md shadow-md mt-2 transition-all duration-300 bg-secondary border border-white/10">
                  {item.sub_menu.map((subItem, subIndex) => (
                    <li key={subIndex}>
@@ -102,7 +102,7 @@ export default function NavBar() {
 
         
         <div className="flex gap-2 items-center" >
-          <button className="sm:hidden block" onClick={() => { setOpen(!open) }} >
+          <button className="md:hidden block" onClick={() => { setOpen(!open) }} >
             {
               open ?
               <CrossIcon className="rotate-45" />:
